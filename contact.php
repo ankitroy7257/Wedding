5ea05a951f6be93241d05c8dac8f76648c9cb6b4
ANKIT KUMAR <98220469+ankitroy7257@users.noreply.github.com>
2022-08-09 00:26:37 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,124 @@
+<?php
+
+$conn = mysqli_connect('localhost','root','','contact_form');
+
+if(isset($_POST['send'])){
+
+   $name = $_POST['name'];
+   $email = $_POST['email'];
+   $number = $_POST['number'];
+   $plan = $_POST['plan'];
+   $address = $_POST['address'];
+   $message = $_POST['message'];
+
+   $insert = "INSERT INTO `contact_form`(`name`, `email`, `number`, `plan`, `address`, `message`) VALUES ('$name','$email','$number','$plan','$address','$message')";
+
+   mysqli_query($conn, $insert);
+
+   header('location:contact.php');
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>contact</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+   <!-- swiper css link  -->
+   <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<div class="container">
+
+<?php @include 'header.php'; ?>
+
+<section class="contact">
+
+   <h1 class="heading">Contact us</h1>
+
+   <form action="" method="post">
+
+      <div class="flex">
+
+         <div class="inputBox">
+            <span>Full Name:</span>
+            <input type="text" placeholder="enter your full name" name="name" required>
+         </div>
+
+         <div class="inputBox">
+            <span>Your Email:</span>
+            <input type="email" placeholder="enter your email" name="email" required>
+         </div>
+
+         <div class="inputBox">
+            <span>Your Number:</span>
+            <input type="number" placeholder="enter your number" name="number" required>
+         </div>
+
+         <div class="inputBox">
+            <span>Choose Plan:</span>
+            <select name="plan">
+            <option value="Select">Selact Your Plan</option>
+               <option value="basic">Basic plan</option>
+               <option value="premium">Premium plan</option>
+               <option value="ultimate">Ultimate plan</option>
+            </select>
+         </div>
+
+         <div class="inputBox">
+            <span>Your Full Address:</span>            
+            <textarea name="address" placeholder="enter your address" required cols="30" rows="10"></textarea>
+         </div>
+
+         <div class="inputBox">
+            <span>Drop Your Message Here:</span>            
+            <textarea name="message" placeholder="enter your message" required cols="30" rows="10"></textarea>
+         </div>
+
+      </div>
+
+      <input type="submit" value="send message" name="send" class="btn">
+
+   </form>
+
+</section>
+
+<?php @include 'footer.php'; ?>
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- swiper js link  -->
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="js/script.js"></script>
+
+</body>
+</html>
---
+++ footer.php
@@ -0,0 +1,45 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>Quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> About</a>
+         <a href="portfolio.php"> <i class="fas fa-angle-right"></i> Portfolio</a>
+         <a href="pricing.php"> <i class="fas fa-angle-right"></i> Pricing</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="#"> <i class="fas fa-angle-right"></i> Plan wedding</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> Our services</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> Ask questions</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> Terms of use</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> Privacy policy</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <a href="#"> <i class="fas fa-phone"></i> 8507166032 </a>
+         <a href="#"> <i class="fas fa-phone"></i> 7257963716 </a>
+         <a href="#"> <i class="fas fa-envelope"></i> ankitkumar66032@gmail.com </a>
+         <a href="#"> <i class="fas fa-envelope"></i> avinash@gmail.com</a>
+         <a href="#"> <i class="fas fa-map"></i> New Delhi, india - 110053 </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="https://www.facebook.com/profile.php?id=100016724381897"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="https://twitter.com/home"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="https://www.instagram.com/_ankit_roy72/"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="https://www.linkedin.com/in/ankit-roy-12807b219/"> <i class="fab fa-linkedin"></i> linkedin </a>
+         <a href="https://github.com/ankitroy7257"> <i class="fab fa-github"></i> github </a>
+      </div>
+
+   </div>
+
+   <p class="credit"> Created by <span>Mr. ANKIT ROY</span> | &copy;2022 All Rights Reserved! </p>
+
+</section>
